d4f2810d20002d22e8f79c7ebdc454df22441b70
sahilvishwakarma018-dev <sahilvishwakarma018@gmail.com>
2025-10-31 16:53:20 +0530

    changed smtp provider host

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST = config('EMAIL_HOST','smtp.sendgrid.net')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # Your Gmail
