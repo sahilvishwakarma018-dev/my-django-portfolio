06f1d882f44d8d3b3a812b113c9e1417e0b2d665
sahilvishwakarma018-dev <sahilvishwakarma018@gmail.com>
2025-12-18 17:41:10 +0530

    configured: Brevo free API configured

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,6 +4,52 @@ from django.core.mail import EmailMessage
 from django.conf import settings
 from .forms import ContactForm
 
+# def home(request):
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             contact = form.save()
+            
+#             # Send email
+#             subject = f'New Portfolio Contact: {contact.name}'
+#             body = f"""
+#             New contact form submission:
+            
+#             Name: {contact.name}
+#             Email: {contact.email}
+#             Message: {contact.message}
+#             """
+            
+#             email = EmailMessage(
+#                 subject=subject,
+#                 body=body,
+#                 from_email=settings.DEFAULT_FROM_EMAIL,
+#                 to=[settings.DEFAULT_FROM_EMAIL],
+#             )
+#             print(settings.DEFAULT_FROM_EMAIL,"-------ddd")
+#             print(settings.EMAIL_HOST_USER,"---uuuu")
+            
+#             if contact.attachment:
+#                 email.attach(
+#                     contact.attachment.name,
+#                     contact.attachment.read(),
+#                     'application/pdf'
+#                 )
+#             try:
+#                 email.send(fail_silently=False)
+#                 messages.success(request, "✅ Message sent successfully!")
+#             except Exception as e:
+#                 print("❌ Email sending failed:", e)
+#                 messages.error(request, "❌ Failed to send message.")
+
+#             return redirect('home')
+#     else:
+#         form = ContactForm()
+    
+#     return render(request, 'portfolio/home.html', {'form': form})
+
+
+
 def home(request):
     if request.method == 'POST':
         form = ContactForm(request.POST, request.FILES)
--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -138,15 +138,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+# EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'sahil.vishwakarma018@gmail.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
+BREVO_API_KEY=os.getenv('BREVO_API_KEY')
 
 # Static and Media Files
 STATIC_URL = '/static/'
